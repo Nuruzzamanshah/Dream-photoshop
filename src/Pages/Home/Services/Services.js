626672d6f968b51b5882b17e1bc7f43e6f589737
Nuruzzamanshah <mnueuzzaman@gmail.com>
2022-04-17 14:44:21 +0600

    section responcive

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Service from '../Service/Service';
+import './Services.css'
 
 const Services = () => {
     const [services, setServices] =useState([]);
@@ -11,15 +12,17 @@ const Services = () => {
     },[])
     return (
         <div>
-            <h1>Our Services</h1>
-            {
-                services.map(service => <Service
-                key={service.id}
-                service={service}
-                >
+            <h1 className='services-title'>Our Services</h1>
+            <div className='services-container'>
+                {
+                    services.map(service => <Service
+                    key={service.id}
+                    service={service}
+                    >
 
-                </Service>)
-            }
+                    </Service>)
+                }
+            </div>
         </div>
     );
 };
