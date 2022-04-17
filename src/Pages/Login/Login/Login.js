93a04090f2480693ef21e71396dfbd0c9b645de4
Nuruzzamanshah <mnueuzzaman@gmail.com>
2022-04-17 21:13:22 +0600

    login and register from added

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,24 +1,39 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Login.css';
 
 const Login = () => {
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
+    const navigate = useNavigate('');
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+
+        console.log(email, password);
+    }
+    const navigateRegister = event => {
+        navigate('/register')
+    }
     return (
         <div>
             <h2 className='text-center mt-3'>Please Login</h2>
-    <div class="login-page">
-            <div class="form">
-                <form class="register-form">
+    <div className="login-page">
+            <div className="form" onSubmit={handleSubmit}>
+                <form className="register-form">
                 <input type="text" placeholder="name"/>
                 <input type="password" placeholder="password"/>
                 <input type="text" placeholder="email address"/>
                 <button>create</button>
-                <p class="message">Already registered? <a href="#">Sign In</a></p>
+                <p className="message">Already registered? <a href="#">Sign In</a></p>
                 </form>
-                <form class="login-form">
-                <input type="email" placeholder="Enter Your Email"/>
-                <input type="password" placeholder="password"/>
+                <form className="login-form">
+                <input ref={emailRef} type="email" placeholder="Enter Your Email" required/>
+                <input ref={passwordRef} type="password" placeholder="password" required/>
                 <button>login</button>
-                <p class="message">Not registered? <a href="#">Create an account</a></p>
+                <p className="message">Not registered? <a href="#" onClick={navigateRegister}>Create an account</a></p>
                 </form>
             </div>
     </div>
--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Register = () => {
+    const navigate = useNavigate();
+    const navigateLogin = () =>{
+        navigate('/login');
+    }
+    const handleRegister = event =>{
+        event.preventDefault();
+        console.log(event.target);
+    }
     return (
         <div>
-            <h3>Please Register</h3>
+            <h3 className='text-center mt-3'>Please Register</h3>
+            <div className="login-page">
+            <div className="form" onSubmit={handleRegister}>
+                <form className="register-form">
+                <input type="text" placeholder="name"/>
+                <input type="password" placeholder="password"/>
+                <input type="text" placeholder="email address"/>
+                <button>create</button>
+                <p className="message">Already registered? <a href="#">Sign In</a></p>
+                </form>
+                <form className="login-form">
+                <input  type="text" placeholder="Enter Your Name" required/>
+                <input  type="email" placeholder="Enter Your Email" required/>
+                <input  type="password" placeholder="password" required/>
+                <button>submit</button>
+                <p className="message">Already have An Account? <a href="login" onClick={navigateLogin}>Please Login</a></p>
+                </form>
+            </div>
+    </div>
         </div>
     );
 };
