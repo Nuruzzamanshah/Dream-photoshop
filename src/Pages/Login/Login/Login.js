5a5b4d4569ae97028d932ce0005cfefcf7392dad
Nuruzzamanshah <mnueuzzaman@gmail.com>
2022-04-18 09:12:37 +0600

    cheakout page added

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,44 +1,67 @@
 import React, { useRef } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Button, Form } from 'react-bootstrap';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import auth from '../../../firebase.init';
 import './Login.css';
 
 const Login = () => {
     const emailRef = useRef('');
     const passwordRef = useRef('');
-    const navigate = useNavigate('');
+    const navigate = useNavigate();
+    const location = useLocation();
+    let from = location.state?.from?.pathname || "/";
+
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useSignInWithEmailAndPassword(auth);
+
+      if(user){
+        navigate(from, { replace: true });
+      }
 
     const handleSubmit = event => {
         event.preventDefault();
         const email = emailRef.current.value;
         const password = passwordRef.current.value;
 
-        console.log(email, password);
+        signInWithEmailAndPassword(email, password);
     }
     const navigateRegister = event => {
         navigate('/register')
     }
     return (
-        <div>
+        
+        <div className='container w-50 mx-auto'>
             <h2 className='text-center mt-3'>Please Login</h2>
-    <div className="login-page">
-            <div className="form" onSubmit={handleSubmit}>
-                <form className="register-form">
-                <input type="text" placeholder="name"/>
-                <input type="password" placeholder="password"/>
-                <input type="text" placeholder="email address"/>
-                <button>create</button>
-                <p className="message">Already registered? <a href="#">Sign In</a></p>
-                </form>
-                <form className="login-form">
-                <input ref={emailRef} type="email" placeholder="Enter Your Email" required/>
-                <input ref={passwordRef} type="password" placeholder="password" required/>
-                <button>login</button>
-                <p className="message">Not registered? <a href="#" onClick={navigateRegister}>Create an account</a></p>
-                </form>
-            </div>
-    </div>
+            
+            <Form onSubmit={handleSubmit}>
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control ref={emailRef} type="email" placeholder="Enter email" required/>
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control ref={passwordRef} type="password" placeholder="Password" required/>
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
+<p>New to Genius Car? <Link to="/register" className='text-primary pe-auto text-decoration-none' onClick={navigateRegister}>Please Register</Link> </p>
         </div>
     );
 };
 
 export default Login;
+
--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,38 +1,57 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
+import './Register.css'
 
 const Register = () => {
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useCreateUserWithEmailAndPassword(auth);
     const navigate = useNavigate();
     const navigateLogin = () =>{
         navigate('/login');
     }
+
+    if(user){
+        navigate('/home');
+    }
     const handleRegister = event =>{
         event.preventDefault();
-        console.log(event.target);
+        // console.log(event.target.email);
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+        createUserWithEmailAndPassword(email, password);
     }
     return (
         <div>
-            <h3 className='text-center mt-3'>Please Register</h3>
-            <div className="login-page">
-            <div className="form" onSubmit={handleRegister}>
-                <form className="register-form">
-                <input type="text" placeholder="name"/>
-                <input type="password" placeholder="password"/>
-                <input type="text" placeholder="email address"/>
-                <button>create</button>
-                <p className="message">Already registered? <a href="#">Sign In</a></p>
-                </form>
-                <form className="login-form">
-                <input  type="text" placeholder="Enter Your Name" required/>
-                <input  type="email" placeholder="Enter Your Email" required/>
-                <input  type="password" placeholder="password" required/>
-                <button>submit</button>
-                <p className="message">Already have An Account? <a href="login" onClick={navigateLogin}>Please Login</a></p>
-                </form>
-            </div>
-    </div>
+            <div className='register-form'>
+            <h2 style={{ textAlign: 'center' }}>Please Register</h2>
+            <form onSubmit={handleRegister}>
+                <input type="text" name="name" id="" placeholder='Your Name' />
+
+                <input type="email" name="email" id="" placeholder='Email Address' required />
+
+                <input type="password" name="password" id="" placeholder='Password' required />
+                <input  type="checkbox" name="terms" id="terms" />
+                {/* <label className={agree ? 'ps-2': 'ps-2 text-danger'} htmlFor="terms">Accept Genius Car Terms and Conditions</label> */}
+                <label htmlFor="terms">Accept Genius Car Terms and Conditions</label>
+                <input
+                className='w-50 mx-auto btn btn-primary mt-2'
+                type="submit"
+                value="Register"
+                />
+            </form>
+            <p>Already have an account? <Link to="/login" className='text-primary pe-auto text-decoration-none' onClick={navigateLogin}>Please Login</Link> </p>
+        </div>
         </div>
     );
 };
 
 export default Register;
+
+
